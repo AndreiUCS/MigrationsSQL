fe155cb6a5b51c3218a1c853c6e1a1b0862c81a5
Ramon Felipe dos Santos <rfsantos8@ucs.local>
2025-10-01 21:52:53 -0300

    feat: inserção de mais dados

---
+++ src/main/resources/db/V3__insert_mais_categorias_1.sql
@@ -0,0 +1,5 @@
+INSERT INTO itens (descricao) VALUES ('Livros');
+INSERT INTO itens (descricao) VALUES ('Brinquedos');
+INSERT INTO itens (descricao) VALUES ('Móveis');
+INSERT INTO itens (descricao) VALUES ('Beleza');
+INSERT INTO itens (descricao) VALUES ('Esportes');
---
+++ src/main/resources/db/V4_insert_mais_categorias_2.sql
@@ -0,0 +1,5 @@
+INSERT INTO itens (descricao) VALUES ('Pet');
+INSERT INTO itens (descricao) VALUES ('Automotivo');
+INSERT INTO itens (descricao) VALUES ('Jardinagem');
+INSERT INTO itens (descricao) VALUES ('Papelaria');
+INSERT INTO itens (descricao) VALUES ('Calçados');
