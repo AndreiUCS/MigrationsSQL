539571fb31d4f320a2ea3925f95d99453b3f6af4
Andrei Fulcher <afribeiro1@ucs.br>
2025-10-01 21:20:53 -0300

    chore: Criação do Projeto Spring Boot

---
+++ src/main/java/br/com/trabalhomigrations/trabalho_migrations/TrabalhoMigrationsApplication.java
@@ -0,0 +1,13 @@
+package br.com.trabalhomigrations.trabalho_migrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoMigrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoMigrationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/trabalhomigrations/trabalho_migrations/TrabalhoMigrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trabalhomigrations.trabalho_migrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoMigrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
